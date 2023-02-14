6970ad0ba81215c6b253fc0dfc5f4e21a3614bac
yakovGrib <100166389+yakovGrib@users.noreply.github.com>
2023-02-14 10:15:05 +0300

    Add files via upload

---
+++ pr7/PractWork5/MainForm.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace PractWork5
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PTextBox = new System.Windows.Forms.TextBox();
+            this.QTextBox = new System.Windows.Forms.TextBox();
+            this.PLabel = new System.Windows.Forms.Label();
+            this.QLabel = new System.Windows.Forms.Label();
+            this.OriginTextBox = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.OriginLabel = new System.Windows.Forms.Label();
+            this.EncryptButton = new System.Windows.Forms.Button();
+            this.DecryptButton = new System.Windows.Forms.Button();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.PathButton = new System.Windows.Forms.Button();
+            this.PurposePathButton = new System.Windows.Forms.Button();
+            this.PurposeLabel = new System.Windows.Forms.Label();
+            this.PurposeTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // PTextBox
+            // 
+            this.PTextBox.Location = new System.Drawing.Point(36, 24);
+            this.PTextBox.Name = "PTextBox";
+            this.PTextBox.Size = new System.Drawing.Size(100, 20);
+            this.PTextBox.TabIndex = 0;
+            this.PTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.PTextBox_KeyPress);
+            // 
+            // QTextBox
+            // 
+            this.QTextBox.Location = new System.Drawing.Point(191, 24);
+            this.QTextBox.Name = "QTextBox";
+            this.QTextBox.Size = new System.Drawing.Size(100, 20);
+            this.QTextBox.TabIndex = 1;
+            this.QTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.QTextBox_KeyPress);
+            // 
+            // PLabel
+            // 
+            this.PLabel.AutoSize = true;
+            this.PLabel.Location = new System.Drawing.Point(33, 8);
+            this.PLabel.Name = "PLabel";
+            this.PLabel.Size = new System.Drawing.Size(13, 13);
+            this.PLabel.TabIndex = 2;
+            this.PLabel.Text = "p";
+            // 
+            // QLabel
+            // 
+            this.QLabel.AutoSize = true;
+            this.QLabel.Location = new System.Drawing.Point(188, 5);
+            this.QLabel.Name = "QLabel";
+            this.QLabel.Size = new System.Drawing.Size(13, 13);
+            this.QLabel.TabIndex = 3;
+            this.QLabel.Text = "q";
+            // 
+            // OriginTextBox
+            // 
+            this.OriginTextBox.Enabled = false;
+            this.OriginTextBox.Location = new System.Drawing.Point(36, 63);
+            this.OriginTextBox.Name = "OriginTextBox";
+            this.OriginTextBox.Size = new System.Drawing.Size(255, 20);
+            this.OriginTextBox.TabIndex = 4;
+            this.OriginTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.OriginTextBox_KeyPress);
+            // 
+            // OriginLabel
+            // 
+            this.OriginLabel.AutoSize = true;
+            this.OriginLabel.Location = new System.Drawing.Point(33, 47);
+            this.OriginLabel.Name = "OriginLabel";
+            this.OriginLabel.Size = new System.Drawing.Size(241, 13);
+            this.OriginLabel.TabIndex = 6;
+            this.OriginLabel.Text = "Выберите путь к директории для шифрования";
+            // 
+            // EncryptButton
+            // 
+            this.EncryptButton.Location = new System.Drawing.Point(36, 128);
+            this.EncryptButton.Name = "EncryptButton";
+            this.EncryptButton.Size = new System.Drawing.Size(100, 23);
+            this.EncryptButton.TabIndex = 8;
+            this.EncryptButton.Text = "Шифровать";
+            this.EncryptButton.UseVisualStyleBackColor = true;
+            this.EncryptButton.Click += new System.EventHandler(this.EncryptButton_Click);
+            // 
+            // DecryptButton
+            // 
+            this.DecryptButton.Location = new System.Drawing.Point(191, 128);
+            this.DecryptButton.Name = "DecryptButton";
+            this.DecryptButton.Size = new System.Drawing.Size(100, 23);
+            this.DecryptButton.TabIndex = 9;
+            this.DecryptButton.Text = "Дешифровать";
+            this.DecryptButton.UseVisualStyleBackColor = true;
+            this.DecryptButton.Click += new System.EventHandler(this.DecryptButton_Click);
+            // 
+            // PathButton
+            // 
+            this.PathButton.Location = new System.Drawing.Point(297, 63);
+            this.PathButton.Name = "PathButton";
+            this.PathButton.Size = new System.Drawing.Size(26, 20);
+            this.PathButton.TabIndex = 10;
+            this.PathButton.Text = "...";
+            this.PathButton.UseVisualStyleBackColor = true;
+            this.PathButton.Click += new System.EventHandler(this.PathButton_Click);
+            // 
+            // PurposePathButton
+            // 
+            this.PurposePathButton.Location = new System.Drawing.Point(297, 102);
+            this.PurposePathButton.Name = "PurposePathButton";
+            this.PurposePathButton.Size = new System.Drawing.Size(26, 20);
+            this.PurposePathButton.TabIndex = 13;
+            this.PurposePathButton.Text = "...";
+            this.PurposePathButton.UseVisualStyleBackColor = true;
+            this.PurposePathButton.Click += new System.EventHandler(this.PurposePathButton_Click);
+            // 
+            // PurposeLabel
+            // 
+            this.PurposeLabel.AutoSize = true;
+            this.PurposeLabel.Location = new System.Drawing.Point(33, 86);
+            this.PurposeLabel.Name = "PurposeLabel";
+            this.PurposeLabel.Size = new System.Drawing.Size(198, 13);
+            this.PurposeLabel.TabIndex = 12;
+            this.PurposeLabel.Text = "Выберите путь к целевой директории";
+            // 
+            // PurposeTextBox
+            // 
+            this.PurposeTextBox.Enabled = false;
+            this.PurposeTextBox.Location = new System.Drawing.Point(36, 102);
+            this.PurposeTextBox.Name = "PurposeTextBox";
+            this.PurposeTextBox.Size = new System.Drawing.Size(255, 20);
+            this.PurposeTextBox.TabIndex = 11;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(350, 163);
+            this.Controls.Add(this.PurposePathButton);
+            this.Controls.Add(this.PurposeLabel);
+            this.Controls.Add(this.PurposeTextBox);
+            this.Controls.Add(this.PathButton);
+            this.Controls.Add(this.DecryptButton);
+            this.Controls.Add(this.EncryptButton);
+            this.Controls.Add(this.OriginLabel);
+            this.Controls.Add(this.OriginTextBox);
+            this.Controls.Add(this.QLabel);
+            this.Controls.Add(this.PLabel);
+            this.Controls.Add(this.QTextBox);
+            this.Controls.Add(this.PTextBox);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(366, 202);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(366, 202);
+            this.Name = "MainForm";
+            this.Text = "Главная RSA";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox PTextBox;
+        private System.Windows.Forms.TextBox QTextBox;
+        private System.Windows.Forms.Label PLabel;
+        private System.Windows.Forms.Label QLabel;
+        private System.Windows.Forms.TextBox OriginTextBox;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label OriginLabel;
+        private System.Windows.Forms.Button EncryptButton;
+        private System.Windows.Forms.Button DecryptButton;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+        private System.Windows.Forms.Button PathButton;
+        private System.Windows.Forms.Button PurposePathButton;
+        private System.Windows.Forms.Label PurposeLabel;
+        private System.Windows.Forms.TextBox PurposeTextBox;
+    }
+}
+
---
+++ pr7/PractWork5/MainForm.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+
+namespace PractWork5
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private bool IsSimple(int n)
+        {
+            if (n < 2) return false;
+            if (n == 2) return true;
+            for (int i = 2; i < n; i++)
+            {
+                if (n % i == 0)
+                    return false;
+            }
+            return true;
+        }
+        int[] openKey;
+        int[] privateKey;
+        string currentPath;
+        string currentPathRes;
+        private void GetKeys()
+        {
+            int p = Convert.ToInt32(PTextBox.Text);
+            int q = Convert.ToInt32(QTextBox.Text);
+
+            if (IsSimple(p) && IsSimple(q))
+            {
+                if (p * q < 255)
+                {
+                    MessageBox.Show("Неккоректные значения, p*q должно быть больше 127", "Ошибка");
+                    return;
+                }
+                if (p * q > 1681)
+                {
+                    MessageBox.Show("Значение p*q слишком большое", "Ошибка");
+                    return;
+                }
+                int n = p * q;
+                int fn = (p - 1) * (q - 1);
+                int exp = 0;
+
+                for (int i = 2; i < fn; i++)
+                {
+                    if (fn % i != 0)
+                    {
+                        exp = i;
+                        break;
+                    }
+                }
+                openKey = new int[2] { exp, n };
+                int d = 0;
+
+                while (true)
+                {
+                    if ((d * exp) % fn == 1)
+                    {
+                        break;
+                    }
+                    d++;
+                }
+                privateKey = new int[2] { d, n };
+            }
+        }
+
+        private void Encrypt(string path)
+        {
+            try
+            {
+                GetKeys();
+                int symb;
+                int siphCounter = 1;
+                byte[] arr;
+                byte[] asciiBytes;
+
+
+                DirectoryInfo d = new DirectoryInfo(currentPath);
+                FileInfo[] files = d.GetFiles("*.*", SearchOption.AllDirectories);
+                foreach (FileInfo file in files)
+                {
+                    arr = File.ReadAllBytes(file.FullName);
+                    asciiBytes = new byte[arr.Length * 2];
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        symb = (int)arr[i];
+                        for (int j = 0; j < openKey[0]; j++)
+                        {
+                            siphCounter = symb * siphCounter % openKey[1];
+                        }
+                        BitConverter.GetBytes((ushort)siphCounter).CopyTo(asciiBytes, i * 2);
+                        siphCounter = 1;
+                    }
+                    File.WriteAllBytes(Path.Combine(currentPathRes, file.Name), asciiBytes);
+                }
+                MessageBox.Show("Файлы зашифрованы", "Поздравляем!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка");
+            }
+
+        }
+
+        private void Decrypt()
+        {
+            byte[] asciiBytesGet;
+            int[] massCiph;
+            int symb;
+            int siphCounter = 1;
+            int[] arrCiph;
+            byte[] asciiBytes;
+
+            try
+            {
+                DirectoryInfo d = new DirectoryInfo(currentPathRes);
+                FileInfo[] files = d.GetFiles("*.*", SearchOption.AllDirectories);
+                foreach (FileInfo file in files)
+                {
+                    asciiBytesGet = File.ReadAllBytes(file.FullName);
+                    massCiph = new int[asciiBytesGet.Length / 2];
+                    arrCiph = new int[massCiph.Length];
+                    asciiBytes = new byte[arrCiph.Length];
+
+                    for (int i = 0; i < asciiBytesGet.Length / 2; i++)
+                    {
+                        massCiph[i] = BitConverter.ToUInt16(asciiBytesGet, i * 2);
+                    }
+
+                    //дешифрование
+                    for (int i = 0; i < massCiph.Length; i++)
+                    {
+                        symb = massCiph[i];
+                        for (int j = 0; j < privateKey[0]; j++)
+                        {
+                            siphCounter = symb * siphCounter % privateKey[1];
+                        }
+                        arrCiph[i] = siphCounter;
+                        siphCounter = 1;
+                    }
+
+                    for (int i = 0; i < arrCiph.Length; i++)
+                    {
+                        asciiBytes[i] = Convert.ToByte(arrCiph[i]);
+                    }
+                    File.WriteAllBytes(file.FullName, asciiBytes);
+                }
+                MessageBox.Show("Файлы расшифрованы", "Поздравляем!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка");
+            }
+        }
+
+        private void EncryptButton_Click(object sender, EventArgs e)
+        {
+
+            Encrypt(currentPath);
+        }
+
+        private void DecryptButton_Click(object sender, EventArgs e)
+        {
+            Decrypt();
+        }
+
+        private void PathButton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                currentPath = OriginTextBox.Text = dialog.SelectedPath;
+            }
+        }
+        private void PurposePathButton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                currentPathRes = PurposeTextBox.Text = dialog.SelectedPath;
+
+            }
+        }
+
+        private void PTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= '0' && e.KeyChar <= '9') || e.KeyChar == (char)Keys.Back)
+            {
+
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void QTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= '0' && e.KeyChar <= '9') || e.KeyChar == (char)Keys.Back)
+            {
+
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void OriginTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= 'A' && e.KeyChar <= 'Z') || (e.KeyChar >= 'a' && e.KeyChar <= 'z') || (e.KeyChar >= '0' && e.KeyChar <= '9') || e.KeyChar == (char)Keys.Back)
+            {
+
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+
+
+    }
+}
